a3e7ef5b726029787a947421c2491a3ff7192f63
anders-kolstad <anders-lk@hotmail.com>
2025-10-08 13:59:01 +0200

    trying to create version badge on the fly

--- _common.R
+++ _common.R
@@ -1,32 +1,27 @@
 # use results: "asis" when setting a status for a chapter
 status <- function(type) {
-  status <- switch(type,
-                   complete = "complete, and indicator values can be calculated as described below.",
-                   incomplete = "incomplete and needs further developement before indicator values can be calculated.",
-                   deprecated = "describing an indicator that is deprecated.",
+  image_path <- switch(type,
+                   complete = "../../../badge_status_operational.svg",
+                   incomplete = "../../../badge_status_under_development.svg",
+                   deprecated = "../../../badge_status_deprecated.svg",
                    stop("Invalid `type`", call. = FALSE)
   )
   
-  class <- switch(type,
-                  complete = "note",
-                  incomplete = "warning",
-                  deprecated = "important"
-  )
-  
-  color <- switch(type,
-                  complete = "lightgreen",
-                  incomplete = "orange",
-                  deprecated = "salmon"
-  )
-  
-  cat(paste0(
-    "\n",
-    ':::  {.callout-', class, ' style="background: ', color,  ';"}', " \n",
-    "## Status",  " \n",
-    "This indicator documentation is ",
-    status,
-    "\n",
-    ":::\n"
-  ))
+  image_link(image_path, "https://github.com/NINAnor/ecRxiv/wiki#status-badge")
+
+}
+library(anybadger)
+version_badge <- function(type){
+  anybadger::create_badge("version_badge.svg", label = "Version", value = type, color = "fuchsia")
+  knitr::include_graphics(tmp)
+
 }
 
+# Function to add url to images embedded using knitr
+
+image_link <- function(image,url,...){
+  htmltools::a(
+    href=url,
+    htmltools::img(src=image,...)
+    )
+}
