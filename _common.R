259f293eb0abca37c45f5eeb955ddc2f63ef3b7e
anders-kolstad <anders-lk@hotmail.com>
2025-10-31 10:55:42 +0100

    Put legacy functions back into _common.R Fixes #238

--- _common.R
+++ _common.R
@@ -101,3 +101,35 @@ get_file_info <- function() {
   return(NULL)
 }
 
+# This is a lagacy function that is not used in the current verion of the qmd template
+# use results: "asis" when setting a status for a chapter
+status <- function(type) {
+  status <- switch(type,
+                   complete = "complete, and indicator values can be calculated as described below.",
+                   incomplete = "incomplete and needs further developement before indicator values can be calculated.",
+                   deprecated = "describing an indicator that is deprecated.",
+                   stop("Invalid `type`", call. = FALSE)
+  )
+  
+  class <- switch(type,
+                  complete = "note",
+                  incomplete = "warning",
+                  deprecated = "important"
+  )
+  
+  color <- switch(type,
+                  complete = "lightgreen",
+                  incomplete = "orange",
+                  deprecated = "salmon"
+  )
+  
+  cat(paste0(
+    "\n",
+    ':::  {.callout-', class, ' style="background: ', color,  ';"}', " \n",
+    "## Status",  " \n",
+    "This indicator documentation is ",
+    status,
+    "\n",
+    ":::\n"
+  ))
+}
